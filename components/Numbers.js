e9359b2c684cec1e2124b5ec9cfe18344b9cec7d
Sam <sam@shbgm.ca>
2019-05-27 12:18:20 -0400

    Design (#2)
    * main page layout
    
    * add components directory
    
    * major design and content

--- components/Numbers.js
+++ components/Numbers.js
@@ -1,25 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledNumbers = styled.div``;
+const StyledNumbers = styled.div`
+  font-size: 0.8rem;
+  padding: 5px;
+  margin-bottom: 5px;
+  display: grid;
+  border: 0.5px solid ${props => props.theme.purpleAccent};
+  border-radius: 5px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  @media only screen and (min-width: 600px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr;
+  }
+`;
 
-const StyledNumber = styled.span``;
+const StyledNumber = styled.span`
+  padding-left: 5px;
+  font-size: 1rem;
+  font-weight: bold;
+  font-family: "Poppins";
+  color: ${props => props.theme.redAccent};
+  @media only screen and (min-width: 600px) {
+    font-size: 0.8rem;
+  }
+`;
 
-const StyledDetail = styled.span``;
+const StyledDetail = styled.span`
+  font-family: "Montserrat", sans-serif;
+`;
 
 const Number = ({ number, detail }) => (
-  <>
+  <div>
     <StyledNumber>{number}</StyledNumber> <StyledDetail>{detail}</StyledDetail>
-  </>
+  </div>
 );
 
 export default () => (
   <>
     <StyledNumbers>
-      <Number number="10" detail="Teams Built" />
-      <Number number="10" detail="Teams Built" />
-      <Number number="10" detail="Teams Built" />
-      <Number number="10" detail="Teams Built" />
+      <Number number="8" detail="Great Teams Built" />
+      <Number number="20" detail="Direct Reports Developed" />
+      <Number number="12+" detail="Successful Projects" />
+      <Number number="Millions" detail="of Users Impacted" />
     </StyledNumbers>
   </>
 );
