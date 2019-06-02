0088f4ce6abf7c1fab9a6c37a4e1e37be68c2e42
Sam <sam@shbgm.ca>
2019-06-02 12:18:26 -0400

    Apollo server (#3)
    * main page layout
    
    * add components directory
    
    * major design and content
    
    * add lambda function
    
    * function build pipeline
    
    * first attempt at working apollo server
    
    * connect server to next js
    
    * update server to lambda
    
    * add new to constructor
    
    * update server
    
    * server update
    
    * server
    
    * update nvmrc
    
    * this time
    
    * trying with no babelization
    
    * remove the damn class
    
    * trying this
    
    * first failed attempts at rest to gql
    
    * working apollo server
    
    * add functions folder
    
    * remove hidden server file
    
    * server function
    
    * Add Contentful and GraphQL Schema
    
    This commit adds a contentful lambda function WIP and a Schema. It isn't
    actually working right now.
    
    * almost got it
    
    * ITS ALIIIIVE

---
+++ functions/server.js
@@ -0,0 +1,97 @@
+const { ApolloServer, gql } = require("apollo-server-lambda");
+const content = require("../lib/contentful.js");
+
+const typeDefs = gql`
+  type Highlight {
+    title: String
+    detail: [String]
+  }
+  type HighlightObject {
+    sys: Sys
+    fields: Highlight
+  }
+  type Job {
+    yearStarted: String
+    role: [String]
+    description: String
+    company: String
+    highlight: [HighlightObject]
+  }
+  type JobObject {
+    sys: Sys
+    fields: Job
+  }
+  type Space {
+    sys: LinkSys
+  }
+  type Environment {
+    sys: LinkSys
+  }
+  type ContentType {
+    sys: LinkSys
+  }
+  type LinkSys {
+    type: String
+    linkType: String
+    id: String
+  }
+  type Sys {
+    id: String
+    revision: Int
+    createdAt: String
+    updatedAt: String
+    type: String
+    locale: String
+    space: Space
+    environment: Environment
+    contentType: ContentType
+  }
+  type Person {
+    name: String
+    headline: String
+    job: [JobObject]
+    flair: String
+    description: String
+  }
+  type PersonObject {
+    sys: Sys
+    fields: Person
+  }
+  type Query {
+    hello: String
+
+    people: [PersonObject]
+    highlights: [HighlightObject]
+    jobs: [JobObject]
+
+    person(id: String!): [Person]
+  }
+
+`;
+
+const resolvers = {
+  Query: {
+    hello: (parent, args, context) => {
+      return "Hello, world!";
+    },
+    people: (parent, args, context) => {
+      return content.people;
+    },
+    person: (parent, args, context) => {
+      return content.person(args.id);
+    },
+    highlights: (parent, args, context) => {
+      return content.highlights;
+    },
+    jobs: (parent, args, context) => {
+      return content.jobs;
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+exports.handler = server.createHandler();
