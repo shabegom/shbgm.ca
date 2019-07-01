520889467486b68faf4de1d31173c3661c18768e
sbgood <sam@sbgood.ca>
2019-07-01 11:27:21 -0400

    come on global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,36 @@
 import App, { Container } from "next/app";
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { PDFExportProvider, PDFExportConsumer } from "../contexts/pdfContext";
 import { PDFExport } from "@progress/kendo-react-pdf";
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'Poppins';
+    src: url('/static/fonts/Poppins.ttf') format('truetype');
+}
+@font-face {
+    font-family: 'Montserrat';
+    src: url('/static/fonts/Montserrat.ttf') format('truetype');
+}
+@font-face {
+    font-family: 'Quicksand';
+    src: url('/static/fonts/Quicksand.ttf') format('truetype');
+}
+@font-face {
+    font-family: 'Yantramanav';
+    src: url('/static/fonts/Yantramanav.ttf') format('truetype');
+}
+         .k-pdf-export .noExport {
+        display: none;
+      } 
+      body {
+  font-family: "Poppins", sans-serif;
+  color: #070707;
+  background-color: FFFAE7;
+      }
+`;
 
 let pdfExportComponent;
 
@@ -52,6 +79,7 @@ export default class MyApp extends App {
                   fileName="Morrison-Samuel-Resume.pdf"
                   title="Samuel Morrison - Resume"
                 >
+                  <GlobalStyle />
                   <Component {...pageProps} />
                 </PDFExport>
               </ThemeProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet, createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-@font-face {
-    font-family: 'Poppins';
-    src: url('/static/fonts/Poppins.ttf') format('truetype');
-}
-@font-face {
-    font-family: 'Montserrat';
-    src: url('/static/fonts/Montserrat.ttf') format('truetype');
-}
-@font-face {
-    font-family: 'Quicksand';
-    src: url('/static/fonts/Quicksand.ttf') format('truetype');
-}
-@font-face {
-    font-family: 'Yantramanav';
-    src: url('/static/fonts/Yantramanav.ttf') format('truetype');
-}
-         .k-pdf-export .noExport {
-        display: none;
-      } 
-      body {
-  font-family: "Poppins", sans-serif;
-  color: #070707;
-  background-color: FFFAE7;
-      }
-`;
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -60,7 +33,6 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
         </Head>
         <body>
-          <GlobalStyle />
           <Main />
           <NextScript />
         </body>
